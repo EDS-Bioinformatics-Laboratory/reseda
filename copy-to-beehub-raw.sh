25b9a5eeffed494ec5150accefe9cc4253f93cf7
B.D.C. van Schaik <b.d.vanschaik@amc.uva.nl>
2015-11-17 12:41:19 +0100

    Template copy-to-beehub.sh scripts

---
+++ copy-to-beehub-raw.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Example URL: myurl="https://beehub.nl/amc-immunogenomics/RUNS/run246/data/"
+# Example URL: myurl="https://beehub.nl/amc-immunogenomics/RUNS/run246/results-tbcell/"
+myurl="https://beehub.nl/amc-immunogenomics/RUNS/runNN-2016MMDD-miseq/results-tbcell/raw/"
+
+#myfiles="runmetrics* *.sff"
+myfiles="*.sam *.snp.csv"
+filelist=`echo $myfiles | perl -ne "@c=split(/\s/); print join(',', @c);"`
+
+starttime=`date +%s`
+
+echo "Calculating checksums... (you need to type the password for Beehub later, so please wait for approx 30 seconds) ..."
+shasum $myfiles > CHECKSUM.$starttime
+wait
+
+echo "Transferring files..."
+curl -T "{$filelist,CHECKSUM.$starttime}" -u bschaik $myurl
+wait
+
+endtime=`date +%s`
+difftime=`expr ${endtime} - ${starttime}`
+
+echo "Finished in $difftime seconds"
+
+# Transfer files FROM beehub (-C - is for resuming files)
+# curl -C - -L -u bschaik -O https://beehub.nl/home/bschaik/immunology/RUNS/run234/data/blah.sff
