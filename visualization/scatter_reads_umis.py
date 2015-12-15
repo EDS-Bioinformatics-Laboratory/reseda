e15b933382e408cd767a0c4e05960f350636a49a
barbera <git@van-schaik.org>
2015-12-15 11:07:24 +0100

    Made it work for multiple clone files at the same time (scatter_reads_umis.py)

--- visualization/scatter_reads_umis.py
+++ visualization/scatter_reads_umis.py
@@ -9,24 +9,37 @@ import matplotlib.pyplot as plt
 if len(sys.argv) < 2:
     sys.exit("Usage: scatter_reads_umis.py clones-subs.csv")
 
-datafile = sys.argv[1]
-plotfile = datafile + ".pdf"
-try:
-    data = np.loadtxt(datafile, delimiter='\t', usecols=(3,4), skiprows=1)
-except:
-    sys.exit("cannot load datafile with numpy")
+datafiles = sys.argv[1:]
 
-print(data)
+def makeScatter (datafile):
+    #plotfile = datafile.split("/")[-1] + ".pdf"
+    plotfile = datafile + ".pdf"
 
-fig, ax = plt.subplots()
-ax.scatter(data[:,[0]], data[:,[1]])
+    try:
+        data = np.loadtxt(datafile, delimiter='\t', usecols=(3,4), skiprows=1)
+    except:
+        sys.exit("cannot load datafile with numpy")
 
-ax.set_xlabel("Reads", fontsize=20)
-ax.set_ylabel("UMI's", fontsize=20)
-ax.set_title(datafile)
+    # print(data)
 
-ax.grid(True)
-fig.tight_layout()
+    fig, ax = plt.subplots()
+    ax.scatter(data[:,[0]], data[:,[1]])
 
-#plt.show()
-plt.savefig(plotfile)
+    ax.set_xlabel("Reads", fontsize=20)
+    ax.set_ylabel("UMI's", fontsize=20)
+    ax.set_title(datafile)
+
+    ax.grid(True)
+    #fig.tight_layout()
+
+    #plt.show()
+    try:
+        plt.savefig(plotfile)
+        print("Wrote", plotfile, "to disk")
+    except:
+        sys.exit("cannot write plotfile to disk")
+
+############# Main #############
+
+for datafile in datafiles:
+    makeScatter(datafile)
