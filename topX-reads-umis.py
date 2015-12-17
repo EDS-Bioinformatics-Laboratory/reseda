2acbadb1d4a4bb227bd63215f21b2e4605eabea5
B.D.C. van Schaik <b.d.vanschaik@amc.uva.nl>
2015-12-17 15:45:07 +0100

    First attempt to put clone info in database, extract the top X clones and make barcharts of the results. Not quite right yet.

--- topX-reads-umis.py
+++ topX-reads-umis.py
@@ -1,7 +1,49 @@
 from __future__ import print_function
-import sqlite3
 import sys
+import sqlite3
 
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Input
+if len(sys.argv)<2:
+    sys.exit("Usage: topX-reads-umis.py *-clones-subs.csv")
+clonesFiles = sys.argv[1:]
+
+########## Functions ###########
+
+def create_table (name, colnames):
+    '''
+    Description: create a table in the database
+    In: name (table name), colnames (list with column names)
+    Out: query (string)
+    '''
+    query = "CREATE TABLE " + name + " ("
+    query = query + ", ".join(colnames)
+    query = query + ");"
+    print(query)
+    cur.execute(query)
+    return(query)
+
+def import_data (datafile, delim, table, colnames):
+    '''
+    Description: Reads a csv file and inserts the content in a table
+    In: datafile (path), delim (e.g. ',' or ' '), table (name of the table in database)
+    Out: query (string)
+    '''
+    fh = open(datafile, 'r')
+    header = fh.readline()  # skip first line
+    for row in fh:
+        row = row.rstrip()
+        row = row.split(delim)
+        query = "INSERT INTO " + table + " ("
+        query = query + ", ".join(colnames)
+        query = query + ") VALUES ("
+        query = query + ", ".join(len(colnames) * ["?"]) + ");"
+        cur.execute(query, row)
+    con.commit()
+    fh.close()
 
 def compareTop (n):
     '''
@@ -32,11 +74,53 @@ def compareTop (n):
 
     return(perc_in_common)
 
+def makeBarChart (x,y):
+    y_pos = np.arange(len(x))
+     
+    plt.bar(y_pos, y, align='center', alpha=0.5)
+    plt.xticks(y_pos, x)
+    plt.xlabel('Top X clones')
+    plt.ylabel('Common clones in topX UMIs versus reads (%)')
+    plt.title(sampleName)
+     
+    try:
+        plt.savefig(plotfile)
+        print("Wrote", plotfile, "to disk")
+        plt.close(plotfile)
+    except:
+        sys.exit("cannot write plotfile to disk")
+
 ########### Main ###########
 
-#con = sqlite3.connect(":memory:")
-con = sqlite3.connect("test.db")
-cur = con.cursor()
+for clonesFile in clonesFiles:
+    # clonesFile = "/mnt/immunogenomics/RUNS/run04-20151116-miseq/test-reads-vs-umis/SP-CB16_S49_L001.assembled-ATGCATGC-IGH_HUMAN-clones-subs.csv"
+    sampleName = clonesFile.split("/")[-1]
+    plotfile = sampleName + "-topX.png"
 
-for n in [1, 5, 10, 25, 50, 100]:
-    print(n, compareTop(n))
+    con = sqlite3.connect(":memory:")
+    #con = sqlite3.connect("test.db")
+    cur = con.cursor()
+
+    # Get header and close the file again
+    try:
+        fh = open(clonesFile, "rb")
+    except:
+        sys.exit("cannot open file")
+    header = fh.readline()
+    fh.close()
+
+    # Create table and fill table with data
+    colnames = header.split()
+    create_table("clones",colnames)
+    import_data(clonesFile, "\t", "clones", colnames)
+
+    # Determine top X clones based on read-frequency and on umi-count
+    x = [1,5,10,25,50,100]
+    y = list()
+    for n in x:
+        y.append(compareTop(n))
+
+    # Make barchart
+    makeBarChart(x,y)
+
+    con.close()
