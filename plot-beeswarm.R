f8b2f0f342d5f5ddd456d19e0af1c04042b64114
B.D.C. van Schaik <b.d.vanschaik@amc.uva.nl>
2016-03-23 16:40:18 +0100

    Count uitzetten in beeswarm plot.

--- plot-beeswarm.R
+++ plot-beeswarm.R
@@ -1,27 +1,27 @@
 library('beeswarm')
 
-myfile="/home/barbera/git/lineage-tree/beeswarm10.png.csv"
-d<-read.csv(myfile, sep=" ", header = FALSE)
+myfile="/home/barbera/git/lineage-tree/beeswarm2.png.csv"
+d<-read.table(myfile, sep=" ", header = FALSE, stringsAsFactors=FALSE)
 colnames(d)<-c("seq","freq","frac","color","accs")
-d$sample="D10"
+d$sample="D2"
 attach(d)
 
 # Enlarge margins: mar=c(bottom, left, top, right)
 # Allow to draw outside the plot region: xpd=TRUE
 # vertical axis labels: las=2
-par(mar=c(8,4,4,2), xpd=TRUE)
+#par(mar=c(8,4,4,2), xpd=TRUE)
 
 # Make the beeswarm plot
-beeswarm(frac ~ sample, data=d,
+beeswarm(freq ~ sample, data=d,
          log = TRUE, pch = 16,
          pwcol=color,
          main = 'Subclones', method="swarm", corral="wrap", corralWidth=0.7, # was 0.9
          las=2)
 # Add legend
-coord<-locator(1)
+#coord<-locator(1)
 #legend(coord$x,coord$y, legend = c("any","GEM1","MAIT","GEM2"),
 #       title = "Cell types", pch = 16, col = c("#00000050",2,3,4),cex=0.75)
-legend(coord$x,coord$y, legend = c("GEM1","MAIT","GEM2"),
-       title = "Cell types", pch = 16, col = c(2,3,4),cex=0.75)
+#legend(coord$x,coord$y, legend = c("GEM1","MAIT","GEM2"),
+#       title = "Cell types", pch = 16, col = c(2,3,4),cex=0.75)
 
-par(def.par)#- reset to default
+#par(def.par)#- reset to default
