07f4d2c659077a006cf4558948f5e47b699c00a7
B.D.C. van Schaik <b.d.vanschaik@amc.uva.nl>
2016-03-31 16:04:49 +0200

    Create overview of sequence run in terms of reads and percentages after each analysis step. To do: add assembly step

--- report-combine-all.py
+++ report-combine-all.py
@@ -120,8 +120,7 @@ def parseReassign (totalreads, summary_mids, reassign):
 
     return(summary)
 
-def makeBarChart (x,y,z,a,b):
-    plotfile = "report-all.pdf"
+def makeBarChart (plotfile,title,y_label, threshold,x,y,z,a,b):
     x_pos = np.arange(len(x))
     
     fig, ax = plt.subplots(figsize=(60, 10)) 
@@ -131,10 +130,12 @@ def makeBarChart (x,y,z,a,b):
     p.append(ax.bar(x_pos, z, align='center', color="blue"))
     p.append(ax.bar(x_pos, a, align='center', color="yellow"))
     p.append(ax.bar(x_pos, b, align='center', color="green"))
+    ax.plot((0,max(x_pos)),(threshold,threshold), '--', color="black")
+
     plt.xticks(x_pos, x, rotation=90)
     ax.set_xlabel('Samples')
-    ax.set_ylabel('Percentage reads')
-    ax.set_title("Summary")
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
     labels = ['With correct MID', 'CDR3 identified', 'VJ assigned', 'After V re-assignment']
     plt.legend((p[0][0], p[1][0], p[2][0], p[3][0]), labels)
 
@@ -166,6 +167,10 @@ midpercs = list()
 cdr3percs = list()
 prodpercs = list()
 reassigns = list()
+midfreqs = list()
+cdr3freqs = list()
+prodfreqs = list()
+reassignfreqs = list()
 for sample in sorted(totalreads):
     # Print all numbers to file
     total = totalreads[sample]
@@ -175,10 +180,18 @@ for sample in sorted(totalreads):
     (reassignfreq, reassignperc) = summary_reassign.get(sample,(0,0))
     print(sample, total, mid, midfreq, midperc, cdr3freq, cdr3perc, prodfreq, prodperc, reassignfreq, reassignperc, file=fhOut)
 
+    # Store percentages in lists
     samples.append(sample)
     midpercs.append(midperc)
     cdr3percs.append(cdr3perc)
     prodpercs.append(prodperc)
     reassigns.append(reassignperc)
 
-makeBarChart(samples,midpercs,cdr3percs,prodpercs,reassigns)
+    # Store frequencies in lists
+    midfreqs.append(midfreq)
+    cdr3freqs.append(cdr3freq)
+    prodfreqs.append(prodfreq)
+    reassignfreqs.append(reassignfreq)
+
+makeBarChart("report-all-percentages.pdf", "Run06", "Reads (percentage)", 70, samples,midpercs,cdr3percs,prodpercs,reassigns)
+makeBarChart("report-all-frequencies.pdf", "Run06", "Reads (frequency)", 50000, samples,midfreqs,cdr3freqs,prodfreqs,reassignfreqs)
