de8ca8379933b2188faab42162f79edf33f3bf44
barbera <git@van-schaik.org>
2016-04-04 23:57:01 +0200

    Added labels with nr of reads

--- plot-v-gene-assignment.py
+++ plot-v-gene-assignment.py
@@ -3,10 +3,10 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-# myfile = "/home/barbera/TMP/S074-299-BCRh_S22_L001.assembled-ACTGACTG-IGH_HUMAN-all_info.csv.rr.csv"
-# myfile = "/home/barbera/TMP/S074-235_S230_L001.assembled-CGTACGTA-TRB_HUMAN-all_info.csv.rr.csv"
-# myfile = "/home/barbera/TMP/S074-002_S73_L001.assembled-ACGTACGT-TRB_HUMAN-all_info.csv.rr.csv"
-myfile = "/home/barbera/TMP/S074-228_S223_L001.assembled-ACGTACGT-TRB_HUMAN-all_info.csv.rr.csv"
+myfile = "/home/narya/TMP/S074-299-BCRh_S22_L001.assembled-ACTGACTG-IGH_HUMAN-all_info.csv.rr.csv"
+# myfile = "/home/narya/TMP/S074-235_S230_L001.assembled-CGTACGTA-TRB_HUMAN-all_info.csv.rr.csv"
+# myfile = "/home/narya/TMP/S074-002_S73_L001.assembled-ACGTACGT-TRB_HUMAN-all_info.csv.rr.csv"
+# myfile = "/home/narya/TMP/S074-228_S223_L001.assembled-ACGTACGT-TRB_HUMAN-all_info.csv.rr.csv"
 
 try:
     fh = open(myfile)
@@ -15,12 +15,22 @@ except:
 
 top = 10
 
+def autolabel(rects, labels):
+    # attach some text labels
+    i = 0
+    for rect in rects:
+        height = rect.get_height()
+        # ax.text(rect.get_x() + rect.get_width()/2., 1.05*height, '%d' % int(height), ha='center', va='bottom')
+        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height, '%d' % labels[i], ha='center', va='bottom', rotation=90)
+        i += 1
+
 # Read fractions and put it in list of list
 header = fh.readline()
 entries = list()        # to store all the data in
 for i in range(top):
     entries.append(list())
 cdr3s = list()
+freqs = list()
 
 n = 0
 first = list()    # store fractions of V gene that has highest occurrence
@@ -54,6 +64,9 @@ for line in fh:
             entries[i].append(0)
 
     cdr3s.append(c[0])
+    freqs_tmp = c[3].split(",")
+    freqs_tmp = [int(freq) for freq in freqs_tmp]
+    freqs.append(sum(freqs_tmp))
     n += 1
 
 fh.close()
@@ -80,16 +93,6 @@ plt.yticks(np.arange(0, 1.1, 0.1))
 labels = ['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh', 'Eighth', 'Nineth', 'Rest']
 labels.reverse()
 plt.legend((p[0][0], p[1][0], p[2][0], p[3][0], p[4][0], p[5][0], p[6][0], p[7][0], p[8][0], p[9][0]), labels)
+autolabel(p[0], freqs)
+
 plt.savefig(myfile + ".stackedbars.svg")
-
-# # Now plot histogram of fractions of the most frequent V gene
-# fig, ax = plt.subplots(figsize=(20, 10)) 
-# hist, bins = np.histogram(first, bins=50)
-# width = 0.7 * (bins[1] - bins[0])
-# center = (bins[:-1] + bins[1:]) / 2
-# plt.bar(center, hist, align='center', width=width)
-# plt.xticks(np.arange(0,1.1,0.1))
-# plt.savefig(myfile + ".histogram.svg")
-
-
-# plt.show()
