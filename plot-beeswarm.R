8013034e547656abf6676fcb08e545b7d36fac4b
B.D.C. van Schaik <b.d.vanschaik@amc.uva.nl>
2016-04-13 14:26:46 +0200

    Plot multiple samples in one beeswarm plot

--- plot-beeswarm.R
+++ plot-beeswarm.R
@@ -1,9 +1,24 @@
 library('beeswarm')
 
-myfile="/home/barbera/git/lineage-tree/beeswarm2.png.csv"
-d<-read.table(myfile, sep=" ", header = FALSE, stringsAsFactors=FALSE)
-colnames(d)<-c("seq","freq","frac","color","accs")
-d$sample="D2"
+# VOOR ANTOINE: B cell lineages
+#myfile="/home/barbera/git/lineage-tree/voor-antoine-b-cell-lineage/LN25-top3-lineages/V3.74-J4/beeswarm2.png.csv"
+#d<-read.table(myfile, sep=" ", header = FALSE, stringsAsFactors=FALSE)
+#colnames(d)<-c("seq","freq","frac","color","accs")
+#d$sample="D2"
+#attach(d)
+
+# VOOR PIPELINE PAPER: clone figuren voor en na V re-assignment
+d1<-read.table("/home/barbera/TMP/S074-002_S73_L001.assembled-ACGTACGT-TRB_HUMAN-clones-subs.csv", sep="\t", header=TRUE, stringsAsFactors=FALSE)
+d1$sample<-"S73-before"
+d2<-read.table("/home/barbera/TMP/S074-002_S73_L001.assembled-ACGTACGT-TRB_HUMAN-all_info.csv.rr.clones_subs.csv", sep="\t", header=TRUE, stringsAsFactors=FALSE)
+d2$sample<-"S73-after"
+d3<-read.table("/home/barbera/TMP/S074-004_S74_L001.assembled-ACTGACTG-TRB_HUMAN-clones-subs.csv", sep="\t", header=TRUE, stringsAsFactors=FALSE)
+d3$sample<-"S74-before"
+d4<-read.table("/home/barbera/TMP/S074-004_S74_L001.assembled-ACTGACTG-TRB_HUMAN-all_info.csv.rr.clones_subs.csv", sep="\t", header=TRUE, stringsAsFactors=FALSE)
+d4$sample<-"S74-after"
+
+# Concatenate everything
+d<-rbind(d1,d2,d3,d4)
 attach(d)
 
 # Enlarge margins: mar=c(bottom, left, top, right)
@@ -12,10 +27,10 @@ attach(d)
 #par(mar=c(8,4,4,2), xpd=TRUE)
 
 # Make the beeswarm plot
-beeswarm(freq ~ sample, data=d,
+beeswarm(read_perc ~ sample, data=d,
          log = TRUE, pch = 16,
-         pwcol=color,
-         main = 'Subclones', method="swarm", corral="wrap", corralWidth=0.7, # was 0.9
+#         pwcol=color,
+         main = 'Clones before and after V gene re-assignment', method="swarm", corral="wrap", corralWidth=0.7, # was 0.9
          las=2)
 # Add legend
 #coord<-locator(1)
