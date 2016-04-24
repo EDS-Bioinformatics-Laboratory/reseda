5b5ef06720c04b88b211cbf23f62725ee766b0a9
B.D.C. van Schaik <b.d.vanschaik@amc.uva.nl>
2016-04-24 20:30:00 +0200

    Script to create a figure as in Doorenspleet et al. SampleX vs SampleY and a dotted line on the HEC threshold.

---
+++ plot-xy-clone-overlap.py
@@ -0,0 +1,80 @@
+from __future__ import print_function
+import sys
+import matplotlib.pyplot as plt
+
+# if len(sys.argv) < 2:
+#     sys.exit("Usage: plot-xy-clone-overlap.py runXX-clones.csv")
+
+
+def readSamples (datafile, sampleX, sampleY):
+
+    try:
+        fh = open(datafile)
+    except:
+        sys.exit("cannot load datafile")
+
+    header = fh.readline()
+    header = header.strip().split()
+    c_sample = header.index("Sample")
+    c_v = header.index("V_sub")
+    c_j = header.index("J_sub")
+    c_cdr3 = header.index("cdr3pep")
+    c_freq = header.index("freq")
+    c_perc = header.index("read_perc")
+
+    data = dict()
+
+    # Put all data for sampleX and sampleY in a dictionary
+    for line in fh:
+        c = line.strip().split()
+
+        if c[c_sample] == sampleX:
+            data[(c[c_v], c[c_j], c[c_cdr3])] = data.get((c[c_v], c[c_j], c[c_cdr3]), dict())
+            data[(c[c_v], c[c_j], c[c_cdr3])][c[c_sample]] = float(c[c_perc])
+        elif c[c_sample] == sampleY:
+            data[(c[c_v], c[c_j], c[c_cdr3])] = data.get((c[c_v], c[c_j], c[c_cdr3]), dict())
+            data[(c[c_v], c[c_j], c[c_cdr3])][c[c_sample]] = float(c[c_perc])
+        else:
+            pass
+
+    # Go through dictionary and fill x and y
+    x = list()
+    y = list()
+
+    for clones in data:
+        x.append(data[clones].get(sampleX, 0))
+        y.append(data[clones].get(sampleY, 0))
+
+    fh.close()
+    return(x,y)
+
+def makeScatter (datafile, sampleX, sampleY):
+    plotfile = datafile.split("/")[-1] + "-" + sampleX + "-" + sampleY + ".pdf"
+    # plotfile = datafile + "-" + sampleX + "-" + sampleY + ".pdf"
+
+    threshold = 0.5   # threshold HEC
+    x, y = readSamples(datafile, sampleX, sampleY)
+
+    fig, ax = plt.subplots()
+    ax.scatter(x, y)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+
+    max_xy = max(x+y)
+    ax.plot((0,max_xy),(threshold,threshold), '--', color="red")
+    ax.plot((threshold,threshold),(0,max_xy), '--', color="red")
+
+    ax.set_xlabel(sampleX, fontsize=20)
+    ax.set_ylabel(sampleY, fontsize=20)
+    ax.set_title(datafile)
+
+    ax.grid(True)
+
+    plt.show()
+    # try:
+    #     plt.savefig(plotfile)
+    #     print("Wrote", plotfile, "to disk")
+    # except:
+    #     sys.exit("cannot write plotfile to disk")
+
+makeScatter("/home/barbera/TMP/run06-clones_subs.csv", "S074-061_S104", "S074-062_S105")
--- test-create-and-import-sqlite.py
+++ test-create-and-import-sqlite.py
@@ -2,19 +2,17 @@ from __future__ import print_function
 import sqlite3
 from CreateAndImportClonesSqlite import *
 
-def importData (datafile):
+def importData (tablename, datafile):
     # load all_info into database
 
-    table = "aa_reads"
-    create_and_import(con, cur, table, datafile)
+    create_and_import(con, cur, tablename, datafile)
 
 #con = sqlite3.connect(":memory:")
-con = sqlite3.connect("test.db")
+con = sqlite3.connect("run06.db")
 cur = con.cursor()
 
-datafile = "/home/narya/TMP/20110405_run81_paul_marieke/out_human_Bcells/aa/2016-04-15-AA.reads-81-BCRh-script20130529-.csv"
-importData(datafile)
-
+# importData("aa_reads", "/home/narya/TMP/20110405_run81_paul_marieke/out_human_Bcells/aa/2016-04-15-AA.reads-81-BCRh-script20130529-.csv")
+importData("clones", "/home/barbera/TMP/run06-clones_subs.csv")
 con.close()
 
 print("Done")
