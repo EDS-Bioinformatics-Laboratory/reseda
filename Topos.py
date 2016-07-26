e9858676ac31633af9319c26986f33c07606fc55
B.D.C. van Schaik <b.d.vanschaik@amc.uva.nl>
2016-07-26 14:57:24 +0200

    Made a seperate script to upload files to Topos

--- Topos.py
+++ Topos.py
@@ -27,10 +27,4 @@ def uploadToken (poolname, f):
     return(token)
 
 if __name__ == '__main__':
-    if len(sys.argv)<3:
-        sys.exit("Usage: " + sys.argv[0] + " poolname files-to-upload")
-
-    poolname = sys.argv[1]
-    for myfile in sys.argv[2:]:
-        token = uploadToken(poolname, myfile)
-        print(token)
+    print("Nothing in main")
