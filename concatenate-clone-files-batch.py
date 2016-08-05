51754ffbaa2bf45b3a0ecd6c745e07a8e2ed90fe
B.D.C. van Schaik <b.d.vanschaik@amc.uva.nl>
2016-08-05 13:05:20 +0200

    Batch script to concatenate clone files

---
+++ concatenate-clone-files-batch.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import subprocess
+
+def executeCmd (cmd):
+    '''
+    Description: execute a command and check if everything went well
+    In: command (str)
+    Out: - an exception will be raised when command was not successful
+    '''
+    print("CMD:", cmd)
+    cmd = cmd.split()
+    rc = subprocess.call(cmd)
+    if rc != 0:
+        print("something went wrong with: " + " ".join(cmd))
+
+if __name__ == '__main__':
+    mydir = "/mnt/immunogenomics/RUNS/runNN-00-08-20160727-miseq/final/correct-mid/"
+    celltypes = ["IGH_HUMAN","IGH_MOUSE","IGK_HUMAN","IGK_MOUSE","IGL_HUMAN","IGL_MOUSE","TRA_HUMAN","TRA_MOUSE","TRB_HUMAN","TRB_MOUSE"]
+
+    for celltype in celltypes:
+        cmd = "python concatenate-clone-files.py " + mydir + "*" + celltype + "-clones-subs.csv"
+        executeCmd(cmd)
+        cmd = "mv run-clones_subs.csv run-clones_subs-" + celltype + ".csv"
+        executeCmd(cmd)
+        cmd = "python concatenate-clone-files.py " + mydir + "*" + celltype + "*.rr.clones_subs.csv"
+        executeCmd(cmd)
+        cmd = "mv run-clones_subs.csv run-clones_subs-" + celltype + "-after-reassignment.csv"
+        executeCmd(cmd)
