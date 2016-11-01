ff90107b08cd356f7f9f67bf53ef2e07fa9dd045
B.D.C. van Schaik <b.d.vanschaik@amc.uva.nl>
2016-11-01 12:52:01 +0100

    Alternative location for the fastq files on basespace

--- copy-basespace-data-to-beehub.py
+++ copy-basespace-data-to-beehub.py
@@ -7,11 +7,19 @@ In: fill in the sampledir and beehub url below
 Out: curl commands on stdout
 '''
 
-sampledir = "/data/home/bioinfo/basespace/Runs/RUN06/Samples"
-myurl = "https://beehub.nl/amc-immunogenomics/RUNS/run06-20160306-miseq/data/"
+myurl = "https://beehub.nl/amc-immunogenomics/RUNS/run10-20161031-miseq/data/"
 
-syscall = os.popen("ls " + sampledir + "/*/Files/Data/Intensities/BaseCalls/*.fastq.gz")
+# sampledir = "/data/home/bioinfo/basespace/Runs/RUN06/Samples"
+# syscall = os.popen("ls " + sampledir + "/*/Files/Data/Intensities/BaseCalls/*.fastq.gz")
+# for line in syscall:
+#     line = line.rstrip()
+#     print('curl -T "' + line + '" --netrc', myurl, "; wait")
 
-for line in syscall:
-    line = line.rstrip()
-    print('curl -T "' + line + '" --netrc', myurl, "; wait")
+rootdir = "/data/home/bioinfo/basespace/Projects/"
+mydirs = ["Paired RA", "VDJmouse (2)", "CEA-CEF", "DNA-RNA", "PsA-SpA"]
+for mydir in mydirs:
+    syscall = os.popen("ls " + rootdir+mydir+"/Samples/*/Files/*.fastq.gz")
+
+    for line in syscall:
+        line = line.rstrip()
+        print('curl -T "' + line + '" --netrc', myurl, "; wait")
