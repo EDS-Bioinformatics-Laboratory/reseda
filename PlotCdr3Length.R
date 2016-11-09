4d5bfd7775a320e24033987b39798c05871f29be
barbera <git@van-schaik.org>
2016-11-09 22:12:33 +0100

    Minor changes to PlotCdr3Length.R

--- PlotCdr3Length.R
+++ PlotCdr3Length.R
@@ -1,15 +1,15 @@
 library("plyr")
 
 # Read CDR3 lengths
-cdr3.length.all=read.csv("run06-tcrb-cdr3-length-all-but-reassigned.txt", header = FALSE)
-cdr3.length.reassigned=read.csv("run06-tcrb-cdr3-length-reassigned.txt", header = FALSE)
+cdr3.length.all=read.csv("run06-bcrh-cdr3-length-all-but-reassigned.txt", header = FALSE)
+cdr3.length.reassigned=read.csv("run06-bcrh-cdr3-length-reassigned.txt", header = FALSE)
 colnames(cdr3.length.all)=c("CDR3length")
 colnames(cdr3.length.reassigned)=c("CDR3length")
 
 # Histogram of the CDR3 length
 hist.cdr3.length.all=ddply(cdr3.length.all, .(CDR3length), summarize, freq=length(CDR3length))
 hist.cdr3.length.reassigned=ddply(cdr3.length.reassigned, .(CDR3length), summarize, freq=length(CDR3length))
-plot(hist.cdr3.length.all, type="o", col="blue")
+plot(hist.cdr3.length.all, type="o", col="blue", xlab="CDR3 length", ylab="Frequency")
 lines(hist.cdr3.length.reassigned, type="o", pch=22, lty=2, col="red")
 
 # Combine in one table and calculate fraction of re-assigned
@@ -19,7 +19,8 @@ colnames(hist.combined) = c("CDR3length","all","reassigned")
 hist.combined$frac.reassigned=hist.combined$reassigned/(hist.combined$reassigned+hist.combined$all)
 
 # Discard non info after length 31
-hist.trunc=hist.combined[1:28,]
+hist.trunc=hist.combined[1:28,] # TCR
+hist.trunc=hist.combined[1:38,] # BCR
 
 # Plot re-assigned vs CDR3 length
 plot(hist.trunc$CDR3length,hist.trunc$frac.reassigned, type="o", col="purple", xlab="CDR3 length", ylab="Fraction re-assigned")
