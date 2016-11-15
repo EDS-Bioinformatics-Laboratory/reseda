acc13f3b989b4aa328976ca463341a83b65ada8e
B.D.C. van Schaik <git@van-schaik.org>
2016-11-15 11:23:57 +0100

    define resultsdir in report-ALL.sh

--- report-ALL.sh
+++ report-ALL.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 run="run10-20161031-miseq"
+resultsdir="results-tbcell-nov2016"
 
-files="/mnt/immunogenomics/RUNS/${run}/results-tbcell/reports/*.log"
+files="/mnt/immunogenomics/RUNS/${run}/${resultsdir}/reports/*.log"
 grep '^Assembled reads \.' $files > report-PEAR.txt
 cat report-PEAR.txt
 
-files="/mnt/immunogenomics/RUNS/${run}/results-tbcell/reports/*-midcount.txt"
+files="/mnt/immunogenomics/RUNS/${run}/${resultsdir}/reports/*-midcount.txt"
 more $files > report-MIDs.txt
 cat report-MIDs.txt
 
-files="/mnt/immunogenomics/RUNS/${run}/results-tbcell/reports/*HUMAN-report.txt /mnt/immunogenomics/RUNS/${run}/results-tbcell/reports/*MOUSE-report.txt"
+files="/mnt/immunogenomics/RUNS/${run}/${resultsdir}/reports/*HUMAN-report.txt /mnt/immunogenomics/RUNS/${run}/${resultsdir}/reports/*MOUSE-report.txt"
 grep '^4' $files > report-CDR3.txt
 cat report-CDR3.txt
 
-files="/mnt/immunogenomics/RUNS/${run}/results-tbcell/reports/*-productive.txt"
+files="/mnt/immunogenomics/RUNS/${run}/${resultsdir}/reports/*-productive.txt"
 grep 'Unique reads with V and J in all_info' $files > report-PRODUCTIVE.txt
 cat report-PRODUCTIVE.txt
 
-python report-after-v-reassignment.py /mnt/immunogenomics/RUNS/${run}/results-tbcell/final/correct-mid/*.rr.clones_subs.csv
+python report-after-v-reassignment.py /mnt/immunogenomics/RUNS/${run}/${resultsdir}/final/correct-mid/*.rr.clones_subs.csv
 cat report-AFTER-V-REASSIGNMENT.txt
 
 python report-combine-all.py
