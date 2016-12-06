b8eeb7d599ea0733ea7c0b77c453fbd7a44b0f7b
B.D.C. van Schaik <git@van-schaik.org>
2016-12-06 17:35:32 +0100

    Fixed problem of resolving short ambiguous HLA names (see e-mail of Giulia on 1 December 2016)

--- TrimHlaNames.py
+++ TrimHlaNames.py
@@ -1,6 +1,26 @@
 from __future__ import print_function
 import sys
+import os.path
 from AmbiguousHlaAlleles import getLookupTable
+
+def readParsedEBI(f):
+    '''
+    Description: Read file and put in a dictionary
+    In: f (filename, e.g. "EBI-ambiguous-alleles-3.26.0.txt")
+    Out: lookupTable[ambig_name] = group_name
+    '''
+    try:
+        fh = open(f)
+    except:
+        sys.exit("cannot open EBI file")
+
+    lookupTable = dict()
+    for line in fh:
+        line = line.strip()
+        (ambig_name, group_name) = line.split()
+        lookupTable[ambig_name] = group_name
+    fh.close()
+    return(lookupTable)
 
 def parseHlaName (oldname):
     '''
@@ -53,7 +73,13 @@ def trimNames (infile, lookupTable):
 
         # Lookup the main name of the gene in case it is an ambiguous name
         # If it is not ambiguous use the original name
-        hlaname_new = lookupTable.get(hlaname_new, hlaname_new)
+        if hlaname_new in lookupTable:
+            hlaname_new = lookupTable[hlaname_new]
+        else:
+            for ambig_name in lookupTable:
+                if ambig_name.startswith(hlaname_new):
+                    print("Ambig name", ambig_name, "startswith observed name", hlaname_new)
+                    hlaname_new = lookupTable[ambig_name]
 
         # Trim the HLA name to 6 digits and sum the frequency
         hlaname_new = trimHlaName(hlaname_new)
@@ -70,7 +96,10 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         sys.exit("Usage: " + sys.argv[0] + " *.hla.count.txt")
 
-    lookupTable = getLookupTable("/home/barbera/git/IMGTHLA/xml/hla_ambigs.xml")
+    if os.path.exists("EBI-ambiguous-alleles-3.26.0.txt"):
+        lookupTable = readParsedEBI("EBI-ambiguous-alleles-3.26.0.txt")
+    else:
+        lookupTable = getLookupTable("/home/barbera/git/IMGTHLA/xml/hla_ambigs.xml")
 
     for infile in sys.argv[1:]:
         outfile = trimNames(infile, lookupTable)
