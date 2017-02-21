6e538a2d510db79e642895e8d4ad1772097a951b
B.D.C. van Schaik <git@van-schaik.org>
2017-02-21 15:18:28 +0100

    SampleSheetRead.py: opens xls and xlsx files and checks if a set of search terms is present. If so, it prints the row(s)

--- SampleSheetRead.py
+++ SampleSheetRead.py
@@ -1,6 +1,7 @@
 import openpyxl as xlsx
 import xlrd as xls
 import sys
+import os
 
 
 def findInRow(p, s):
@@ -20,10 +21,18 @@ def readXlsx(f, p):
     # DOC: https://openpyxl.readthedocs.io/en/default/tutorial.html#loading-from-a-file
     wb = xlsx.load_workbook(f)
     sheets = wb.get_sheet_names()
-    print(sheets)
-    for s in sheets:
-        sheet_ranges = wb[s]
-        print(s, sheet_ranges.max_row, 'x', sheet_ranges.max_column)
+    # print(sheets)
+    for sheet_name in sheets:
+        s = wb[sheet_name]
+        # print(sheet_name, s.max_row, 'x', s.max_column)
+
+        for row in s.rows:
+            values = list()
+            for cell in row:
+                values.append(str(cell.value))
+            myrow = ','.join(values)
+            if findInRow(p, myrow) is True:
+                print(f, sheet_name, myrow)
 
 
 def readXls(f, p):
@@ -32,36 +41,40 @@ def readXls(f, p):
     sheets = list()
     for s in wb.sheets():
         sheets.append(s.name)
-        print(s.name, s.nrows, 'x', s.ncols)
+        # print(s.name, s.nrows, 'x', s.ncols)
 
         for row in range(s.nrows):
             values = list()
             for col in range(s.ncols):
                 values.append(str(s.cell(row, col).value))
-
-        myrow = ','.join(values)
-        if findInRow(p, myrow) is True:
-            print(myrow)
-
-    print(sheets)
+            myrow = ','.join(values)
+            if findInRow(p, myrow) is True:
+                print(f, s.name, myrow)
 
 
 if __name__ == '__main__':
-    # Check extension: .xls .xlsx .csv and use the appropriate package to open the file
-    f = sys.argv[1]
-    ext = f.split(".")[-1]
+    if len(sys.argv) < 2:
+        sys.exit("Usage: " + sys.argv[0] + "MID-overview-directory-name/")
+    d = sys.argv[1]
+    myfiles = os.listdir(d)
+    myfiles.sort()
 
-    p = list()
-    for i in range(25):
-        p.append("HD" + str(i))
-    print("p = ", p)
+    for myfile in myfiles:
+        f = d + myfile
+        # print(f)
 
-    if ext == "xlsx":
-        readXlsx(f, p)
-    elif ext == "xls":
-        readXls(f, p)
-    else:
-        print("Unknown extension:", ext)
+        # Check extension: .xls .xlsx .csv and use the appropriate package to open the file
+        ext = f.split(".")[-1]
 
-    # Open each sheet and search for a particular term (or terms)
-    # Print the entire row if search term is found
+        p = list()
+        for i in range(25):
+            p.append("HD" + str(i))
+        # print("p = ", p)
+
+        if ext == "xlsx":
+            readXlsx(f, p)
+        elif ext == "xls":
+            readXls(f, p)
+        else:
+            pass
+            # print("Unknown extension:", ext)
