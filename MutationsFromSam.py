fec7794868f38691724416613ec13173b24e7af8
B.D.C. van Schaik <git@van-schaik.org>
2017-05-04 17:11:47 +0200

    Parse cigar string and convert this to coordinates in the sequence

--- MutationsFromSam.py
+++ MutationsFromSam.py
@@ -2,13 +2,59 @@ from __future__ import print_function
 import sys
 
 
+def parseCigar(cigarstring):
+    '''
+    Description: split cigarstring and store in list of tuples [(number, letter)] and convert it to coordinates in sequence
+    '''
+    c = list()
+    coord = list()
+    number = ""
+    start = 0
+    end = 0
+    for character in cigarstring:
+        try:
+            x = int(character)
+            number += character
+        except:
+            number = int(number)
+            c.append((number, character))
+
+            # convert to coordinates if part of the alignment
+            end = start + number
+            if character == "M":
+                coord.append((start, end))
+            start = end
+
+            # reset number
+            number = ""
+
+    return(coord)
+
+
 def parseSam(f):
     '''
     Description: gets number of mutations in the aligned part of the query sequence
     In: name of sam file
     Out: -
     '''
-    print(f)
+    try:
+        fh = open(f)
+    except:
+        sys.exit("cannot open file: " + f)
+
+    for line in fh:
+        # Skip header
+        if line.startswith("@"):
+            continue
+
+        # Read alignment
+        line = line.rstrip()
+        line = line.split()
+        cigar = parseCigar(line[5])
+        seq = line[9]
+        print(line[5], cigar, seq)
+        for (start, end) in cigar:
+            print("  " + seq[start:end])
 
 
 if __name__ == '__main__':
