3fd7fd841a4dc5754546fbcee84a01e70cc82eea
B.D.C. van Schaik <git@van-schaik.org>
2017-05-09 10:43:48 +0200

    Added: jupyter notebook with the analysis (from execute-all.sh)

--- batch-pear.sh
+++ batch-pear.sh
@@ -8,7 +8,7 @@ for s in $samples; do
     mydir=`dirname $s`
     prefix=`basename $s _R1_001.fastq.gz`
     ./pear-0.9.6-bin-64 -f ${mydir}/${prefix}_R1_001.fastq.gz -r ${mydir}/${prefix}_R2_001.fastq.gz -o ${prefix} > ${prefix}-pear.log 2> ${prefix}-pear.err
-    gzip ${prefix}.assembled.fastq
+    gzip -f ${prefix}.assembled.fastq
 done
 
 endtime=`date +%s`
