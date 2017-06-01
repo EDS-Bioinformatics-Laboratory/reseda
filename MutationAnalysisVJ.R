a2e11fd2d3bb1ea61d3b754d03e5f3da3637c185
B.D.C. van Schaik <git@van-schaik.org>
2017-06-01 10:50:10 +0200

    MutationAnalysisVJ.R: put everything in a function. Combine V and J mutations with CDR3 info

--- MutationAnalysisVJ.R
+++ MutationAnalysisVJ.R
@@ -1,8 +1,10 @@
+library(plyr)
+
 plotMutations<-function(directory, V.file, J.file){
   sample = gsub("(^.+)_L001.*", "\\1", V.file)
   d.V = read.csv(paste(directory,V.file,sep=""), header=T, sep=" ", stringsAsFactors = F)
   d.J = read.csv(paste(directory,J.file,sep=""), header=T, sep=" ", stringsAsFactors = F)
-  
+
   # Remove sequences without mutations
   #d.V = d.V[which(d.V$mut.count>0),]
   #d.J = d.J[which(d.J$mut.count>0),]
@@ -17,18 +19,37 @@ plotMutations<-function(directory, V.file, J.file){
   mtext(sample, side=3, outer=TRUE, line=-3)
   dev.off()
   cat("Wrote", paste(sample, "-mutations.pdf", sep=""), "to disk")
+  
+  return(list(d.V, d.J))
+}
+
+main<-function(directory, V.file, J.file, CDR3.file){
+  # Read and combine data
+  d = plotMutations(directory, V.file, J.file)
+  d.V = d[[1]]
+  d.J = d[[2]]
+  rm(d)  # clean up
+  d.CDR3 = read.csv(CDR3.file, header=T, sep="\t", stringsAsFactors = F)
+  d.CDR3$VJCDR3 = paste(d.CDR3$V_sub, d.CDR3$J_sub, d.CDR3$cdr3pep, sep = "-")
+  d.combined = merge(merge(d.CDR3, d.V, by="acc"), d.J, by="acc")
+
+  # Summarize mutations per VJCDR3 clone
+  clones = ddply(d.combined, .(VJCDR3), summarise, freq=length(acc), total.mut.count.V=sum(mut.count.x), avg.mut.frac.V=mean(mut.frac.x), total.mut.count.J=sum(mut.count.y), avg.mut.frac.J=mean(mut.frac.y))
+  clones = clones[which(clones$freq>1),]            # REMOVE CLONES WITH FREQUENCY==1
+  clones = clones[order(clones$freq, decreasing=T),]
 }
 
 # Read data
 directory = "/mnt/immunogenomics/RUNS/run07-20160401-miseq/results-tbcell/raw/"
 V.file = "SP-Bsort06_S157_L001.assembled-ACGTACGT-IGHV_human-e-clean.sam.mut.txt" # Naive
 J.file = "SP-Bsort06_S157_L001.assembled-ACGTACGT-IGHJ_human-e-clean.sam.mut.txt"
-plotMutations(directory, V.file, J.file)
+CDR3.file = "/mnt/immunogenomics/RUNS/run07-20160401-miseq/results-tbcell/final/correct-mid/SP-Bsort06_S157_L001.assembled-ACGTACGT-IGH_HUMAN-all_info.csv.rr.all_info.csv"
+main(directory, V.file, J.file, CDR3.file)
 
-V.file = "SP-Bsort07_S158_L001.assembled-ACTGACTG-IGHV_human-e-clean.sam.mut.txt" # Memory
-J.file = "SP-Bsort07_S158_L001.assembled-ACTGACTG-IGHJ_human-e-clean.sam.mut.txt"
-plotMutations(directory, V.file, J.file)
+# V.file = "SP-Bsort07_S158_L001.assembled-ACTGACTG-IGHV_human-e-clean.sam.mut.txt" # Memory
+# J.file = "SP-Bsort07_S158_L001.assembled-ACTGACTG-IGHJ_human-e-clean.sam.mut.txt"
+# CDR3.file = "/mnt/immunogenomics/RUNS/run07-20160401-miseq/results-tbcell/final/correct-mid/SP-Bsort07_S158_L001.assembled-ACTGACTG-IGH_HUMAN-all_info.csv.rr.all_info.csv"
 
-V.file = "SP-Bsort09_S160_L001.assembled-ATCGATCG-IGHV_human-e-clean.sam.mut.txt" # Paxgene
-J.file = "SP-Bsort09_S160_L001.assembled-ATCGATCG-IGHJ_human-e-clean.sam.mut.txt"
-plotMutations(directory, V.file, J.file)
+# V.file = "SP-Bsort09_S160_L001.assembled-ATCGATCG-IGHV_human-e-clean.sam.mut.txt" # Paxgene
+# J.file = "SP-Bsort09_S160_L001.assembled-ATCGATCG-IGHJ_human-e-clean.sam.mut.txt"
+# CDR3.file = "/mnt/immunogenomics/RUNS/run07-20160401-miseq/results-tbcell/final/correct-mid/SP-Bsort09_S160_L001.assembled-ATCGATCG-IGH_HUMAN-all_info.csv.rr.all_info.csv"
