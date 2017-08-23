a6d7d0462feefc8dc36bd0e906dd696c592ad142
git@van-schaik.org <git@van-schaik.org>
2017-08-23 17:17:19 +0200

    Make reports for alignments. To do: merge the reports with the other reports

--- report-ALL.sh
+++ report-ALL.sh
@@ -6,22 +6,26 @@ resultsdir="results-tbcell"
 
 files="/mnt/immunogenomics/RUNS/${run}/${resultsdir}/reports/*.log"
 grep '^Assembled reads \.' $files > report-PEAR.txt
-cat report-PEAR.txt
+echo "Wrote report-PEAR.txt"
 
 files="/mnt/immunogenomics/RUNS/${run}/${resultsdir}/reports/*-midcount.txt"
 more $files > report-MIDs.txt
-cat report-MIDs.txt
+echo "Wrote report-MIDs.txt"
 
 files="/mnt/immunogenomics/RUNS/${run}/${resultsdir}/reports/*HUMAN-report.txt /mnt/immunogenomics/RUNS/${run}/${resultsdir}/reports/*MOUSE-report.txt"
 grep '^4' $files > report-CDR3.txt
-cat report-CDR3.txt
+echo "Wrote report-CDR3.txt"
 
 files="/mnt/immunogenomics/RUNS/${run}/${resultsdir}/reports/*-productive.txt"
 grep 'Unique reads with V and J in all_info' $files > report-PRODUCTIVE.txt
-cat report-PRODUCTIVE.txt
+echo "Wrote report-PRODUCTIVE.txt"
+
+files="/mnt/immunogenomics/RUNS/${run}/${resultsdir}/raw/*.sam"
+python report-alignments.py ${files}
+echo "Wrote report-ALIGNED-*"
 
 python2 report-after-v-reassignment.py /mnt/immunogenomics/RUNS/${run}/${resultsdir}/final/correct-mid/*.rr.clones_subs.csv
-cat report-AFTER-V-REASSIGNMENT.txt
+echo "Wrote report-AFTER-V-REASSIGNMENT.txt"
 
 python2 report-combine-all.py ${runinfo}
 wait
