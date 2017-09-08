b698505743f1dd23b18c762335da5671e327a2b3
B.D.C. van Schaik <git@van-schaik.org>
2017-09-08 11:33:02 +0200

    latest version AnalyzeThresholdBasequality.R

--- AnalyzeThresholdBasequality.R
+++ AnalyzeThresholdBasequality.R
@@ -1,17 +1,28 @@
 setwd("/home/barbera/git/lineage-tree/")
 
+# TCRb dataset
+all_info=read.csv("AB-RBF126-T_S92_L001.assembled-ATCGATCG-TRB_HUMAN-all_info.csv.rr.all_info.csv", header=T, sep="\t", stringsAsFactors = F)
 t10=read.csv("AB-RBF126-T_S92_L001.assembled-ATCGATCG-TRB_HUMAN-all_info.csv.rr.all_info.csv.report.10.txt", header=T, sep=" ", stringsAsFactors = F)
 t20=read.csv("AB-RBF126-T_S92_L001.assembled-ATCGATCG-TRB_HUMAN-all_info.csv.rr.all_info.csv.report.20.txt", header=T, sep=" ", stringsAsFactors = F)
 t30=read.csv("AB-RBF126-T_S92_L001.assembled-ATCGATCG-TRB_HUMAN-all_info.csv.rr.all_info.csv.report.30.txt", header=T, sep=" ", stringsAsFactors = F)
 t40=read.csv("AB-RBF126-T_S92_L001.assembled-ATCGATCG-TRB_HUMAN-all_info.csv.rr.all_info.csv.report.40.txt", header=T, sep=" ", stringsAsFactors = F)
 
+# BCRh dataset
+all_info=read.csv("AB-RBF147-B_S143_L001.assembled-ACTGACTG-IGH_HUMAN-all_info.csv.rr.all_info.csv", header=T, sep="\t", stringsAsFactors = F)
 t10=read.csv("AB-RBF147-B_S143_L001.assembled-ACTGACTG-IGH_HUMAN-all_info.csv.rr.all_info.csv.report.10.txt", header=T, sep=" ", stringsAsFactors = F)
 t20=read.csv("AB-RBF147-B_S143_L001.assembled-ACTGACTG-IGH_HUMAN-all_info.csv.rr.all_info.csv.report.20.txt", header=T, sep=" ", stringsAsFactors = F)
 t30=read.csv("AB-RBF147-B_S143_L001.assembled-ACTGACTG-IGH_HUMAN-all_info.csv.rr.all_info.csv.report.30.txt", header=T, sep=" ", stringsAsFactors = F)
 t40=read.csv("AB-RBF147-B_S143_L001.assembled-ACTGACTG-IGH_HUMAN-all_info.csv.rr.all_info.csv.report.40.txt", header=T, sep=" ", stringsAsFactors = F)
 
+# Make figures
+par(mfrow=c(4,2))
+
+# for TCRb
 boxplot(t10$low_qual, t20$low_qual, t30$low_qual, t40$low_qual, names=c("10", "20", "30", "40"), xlab="Base quality threshold", ylab="Read count low quality CDR3")
-boxplot(t10$high_qual, t20$high_qual, t30$high_qual, t40$low_qual, names=c("10", "20", "30", "40"), xlab="Base quality threshold", ylab="Read count high quality CDR3")
+boxplot(t10$high_qual, t20$high_qual, t30$high_qual, t40$high_qual, names=c("10", "20", "30", "40"), xlab="Base quality threshold", ylab="Read count high quality CDR3")
+# for BCRh
+boxplot(t10$low_qual, t20$low_qual, t30$low_qual, names=c("10", "20", "30"), xlab="Base quality threshold", ylab="Read count low quality CDR3")
+boxplot(t10$high_qual, t20$high_qual, t30$high_qual, names=c("10", "20", "30"), xlab="Base quality threshold", ylab="Read count high quality CDR3")
 
 plot(t10$low_qual, t10$high_qual, main=paste("n=",length(t10$CDR3)))
 plot(t20$low_qual, t20$high_qual, main=paste("n=",length(t20$CDR3)))
@@ -24,3 +35,4 @@ c=100 * length(which(t30$related_to_top100 >= 1)) / length(t30$CDR3)
 d=100 * length(which(t40$related_to_top100 >= 1)) / length(t40$CDR3)
 
 barplot(c(a,b,c,d), names=c("10", "20", "30", "40"), xlab="Base quality threshold", ylab = "Has similarity to top 100 cdr3's (%)")
+hist(all_info$cdr3_qual_min, breaks=40)
