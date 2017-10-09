315bb37470be17d4b4259fa877d19c3dc337ca03
B.D.C. van Schaik <git@van-schaik.org>
2017-10-09 14:25:13 +0200

    InSilicoPcr.py: also search for the primer in the complementary reverse direction

--- InSilicoPcr.py
+++ InSilicoPcr.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-import sys
 import argparse
 import regex
 from Bio import SeqIO
@@ -47,18 +46,23 @@ def findPrimers(seq_file, seq_format, patterns):
     fh = open(seq_file)
     for record in SeqIO.parse(fh, seq_format):
         seq = str(record.seq).upper()
+        comrev_seq = sequences.comrev(seq)
         for pattern in patterns:
             p = regex.compile(pattern, regex.BESTMATCH)
             m = p.search(seq)
             if m is not None:
                 (start, end) = m.span(1)
-                print(record.id, pattern, start, end, m.group(1))
+                print(record.id, "+", pattern, start, end, m.group(1))
+            m = p.search(comrev_seq)
+            if m is not None:
+                (start, end) = m.span(1)
+                print(record.id, "-", pattern, start, end, m.group(1))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Perform in-silico PCR')
     parser.add_argument('-fwd', '--forward_primer', default='', type=str, help='Nucleotide sequences separated by comma: CATG,ACGT,ETC (default: %(default)s)')
-    parser.add_argument('-rev', '--reverse_primer', default='', type=str, help='Nucleotide sequence separated by comma: CATG,ACGT,ETC (default: %(default)s)')
+    # parser.add_argument('-rev', '--reverse_primer', default='', type=str, help='Nucleotide sequence separated by comma: CATG,ACGT,ETC (default: %(default)s)')
     parser.add_argument('-m', '--mismatches', default=0, type=int, help='Allowed nr of mismatches (default: %(default)s)')
     parser.add_argument('-f', '--seq_format', default='fasta', type=str, help='Sequence format, currently only fasta is supported (default: %(default)s)')
     parser.add_argument("seq_files", type=str, nargs='+', help='Path(s) to sequence file(s)')
