8089d316d6579aa8671daf473f4c203b3849ea37
Barbera van Schaik <git@van-schaik.org>
2018-02-13 17:17:08 +0100

    Reading a file from a webdav server as a function

--- ConnectToWebdav.R
+++ ConnectToWebdav.R
@@ -2,9 +2,14 @@ library(httr)
 
 # N.B. Create a variable 'passwd' with the password manually
 
-url="https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/run20-20171127-miseq/results-tbcell20171129/report-all.csv"
-fh=GET(url, authenticate("bschaik", passwd))
-
-if (http_status(fh)$reason == "OK") {
-  df <- read.csv(textConnection(content(fh, 'text')), header=T, sep=" ")  
+readRemoteFile<-function(url){
+  fh=GET(url, authenticate("bschaik", passwd))
+  
+  if (http_status(fh)$reason == "OK") {
+    df <- read.csv(textConnection(content(fh, 'text')), header=T, sep=" ")  
+  }
+  return(df)
 }
+
+url="https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/run20-20171127-miseq/results-tbcell20171129/report-all.csv"
+mydata=readRemoteFile(url)
