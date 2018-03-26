c73fc162aa3cfd9f661d17259b535e6e7f86b7c2
git@van-schaik.org <git@van-schaik.org>
2018-03-26 17:33:22 +0200

    ENV.sh: shortcuts to the ResearchDrive

---
+++ ENV.sh
@@ -0,0 +1,4 @@
+# Source this file to have shortcuts to the ResearchDrive
+RUN=run25-20180326-miseq
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
+JSON=blah-new.json
