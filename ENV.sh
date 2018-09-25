b632e5bd91639949d864030f916600890b07754b
Barbera van Schaik <git@van-schaik.org>
2018-09-25 12:50:52 +0200

    Added extra variable to ENV.sh (OUTDIR)

--- ENV.sh
+++ ENV.sh
@@ -1,4 +1,5 @@
 # Source this file to have shortcuts to the ResearchDrive
-RUN=run26-20180409-miseq
+RUN=run25-20180326-miseq
+OUTDIR=AB-RBF20180821
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
 POOLNAME=d8c24f78f9772cbdff54cf62
