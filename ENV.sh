472947402847258eb7491acf6405637ee6f1e570
Barbera van Schaik <git@van-schaik.org>
2018-11-27 16:35:15 +0100

    update ENV.sh

--- ENV.sh
+++ ENV.sh
@@ -1,5 +1,5 @@
 # Source this file to have shortcuts to the ResearchDrive
-RUN=run25-20180326-miseq
-OUTDIR=AB-RBF20180821
+RUN=run32-20181125-miseq
+OUTDIR=results-tbcell
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
 POOLNAME=d8c24f78f9772cbdff54cf62
