20ced7a11a0672ad579d8cfcc49bbc7074c8706e
Barbera DC van Schaik <git@van-schaik.org>
2019-01-29 13:30:01 +0100

    Batch script: copy scripts to /scratch before executing it and remove the directory afterwards

--- tbcell-agent.sh
+++ tbcell-agent.sh
@@ -12,7 +12,24 @@ module load R
 
 export STOPOS_POOL=d8c24f78f9772cbdff54cf62
 
+# Create work directory
+THISDIR=`pwd`
+MYJOBDIR="${TMPDIR}/${SLURM_JOB_USER}-${SLURM_NODELIST}-${SLURM_JOB_ID}"
+echo "MYJOBDIR: ${MYJOBDIR}"
+
+mkdir ${MYJOBDIR}
+cp -r ../tbcell-miseq-pipeline ${MYJOBDIR}/
+cp -r ../progress ${MYJOBDIR}/
+
+# Go to the temporary directory
+cd ${MYJOBDIR}/tbcell-miseq-pipeline
+mv reference/* .
+
 # Start analysis
-python2 StoposGetTokens.py ${STOPOS_POOL} 
+python2 StoposGetTokens.py ${STOPOS_POOL}
+
+# Clean up
+cd ${THISDIR}
+rm -rf ${MYJOBDIR}
 
 echo "FINISHED"
