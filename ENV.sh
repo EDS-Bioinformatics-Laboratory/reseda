e6fee2d409c54427832588bf70888c2de1f2a8c3
git@van-schaik.org <git@van-schaik.org>
2019-06-27 17:37:40 +0200

    Partially cleaned up the repository

--- ENV.sh
+++ ENV.sh
@@ -1,5 +1,5 @@
 # Source this file to have shortcuts to the ResearchDrive
-RUN=run32-20181125-miseq
-OUTDIR=results-tbcell
+RUN=run34-20190218-miseq
+OUTDIR=RHESUS20190626
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
 POOLNAME=d8c24f78f9772cbdff54cf62
