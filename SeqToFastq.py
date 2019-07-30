8e1691215587cc12582ce279266fdbd814b7c275
Barbera van Schaik <git@van-schaik.org>
2019-07-30 12:52:23 +0200

    SeqToFastq.py: added function to convert fasta+qual to fastq

--- SeqToFastq.py
+++ SeqToFastq.py
@@ -56,6 +56,28 @@ def convertFastaToFastq(f):
     return(fastqFile)
 
 
+def convertFastaQualToFastq(f,q):
+    '''
+    Description: convert fasta+qual files to fastq
+    In: file.fasta and file.qual (both str)
+    Out: file.fastq.gz (str)
+    '''
+
+    fastqFile = os.path.basename(f)
+    fastqFile = fastqFile.replace(".seq", ".fastq.gz")
+    try:
+        fhOut = gzip.open(fastqFile, "w")
+    except:
+        print("Could not write file:", fastqFile)
+    reads = SeqIO.to_dict(SeqIO.parse(f, "fasta"))
+    for rec in SeqIO.parse(q, "qual"):
+        reads[rec.id].letter_annotations["phred_quality"]=rec.letter_annotations["phred_quality"]
+        SeqIO.write(reads[rec.id], fhOut, "fastq")
+    fhOut.close()
+
+    return(fastqFile)
+
+
 def convertFastqToFasta(f):
     '''
     Description: convert sequences from fastq to fasta format
@@ -109,11 +131,11 @@ def convertTabToFastq(f):
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        sys.exit("Usage: " + sys.argv[0] + " sff|fasta|tab|fastq2fasta sequence-file(s) Supported formats: sff, fasta, tab, fastq2fasta")
+        sys.exit("Usage: " + sys.argv[0] + " sff|fasta|fastaqual|tab|fastq2fasta sequence-file(s) Supported formats: sff, fasta, fastaqual, tab, fastq2fasta")
 
     input_type = sys.argv[1]
     myfiles = sys.argv[2:]
-    for f in myfiles:
+    for i, f in enumerate(myfiles):
         if input_type == "fasta":
             fastqFile = convertFastaToFastq(f)
         elif input_type == "sff":
@@ -124,6 +146,11 @@ if __name__ == '__main__':
             id_col = 0
             seq_col = 1
             fastqFile = convertTabToFastq(f)
+        elif input_type == "fastaqual":
+            q = myfiles[i + 1]
+            fastqFile = convertFastaQualToFastq(f,q)
+            print("Converted:", f, q, ">", fastqFile)
+            exit(0)
         else:
             sys.exit("Unknown input type: " + input_type)
         print("Converted:", f, ">", fastqFile)
