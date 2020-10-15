ea3d840194954361e1407c9ef9234d08cb199576
Barbera van Schaik <git@van-schaik.org>
2020-10-15 17:57:06 +0200

    Scripts to transfer the SFF>FASTQ converted files to the ResearchDrive. One more dataset to go: SAMPLES-sff-ad

---
+++ NOTEBOOKS/cp-fastq-to-researchdrive-ab.sh
@@ -0,0 +1,35 @@
+RUN=run243
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+RUN=run246
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+RUN=run24
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+RUN=run252
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+RUN=run254
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+RUN=run259
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+RUN=run27
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+RUN=run34
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+RUN=run37
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
---
+++ NOTEBOOKS/cp-fastq-to-researchdrive-ac.sh
@@ -0,0 +1,30 @@
+RUN=run41
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+RUN=run45
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+RUN=run49
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+RUN=run51
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+RUN=run55
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+
+RUN=run58
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+RUN=run61
+WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}/data/
+../copy-to-webdav.sh $WEBDAV ${RUN}-*.fastq.gz
+
+echo "FINISHED"
