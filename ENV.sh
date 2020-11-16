9f30064444210b3e340aa99e98377d8236a1f38c
Barbera van Schaik <git@van-schaik.org>
2020-11-16 12:14:49 +0100

    Info about the data analysis

--- ENV.sh
+++ ENV.sh
@@ -1,5 +1,5 @@
 # Source this file to have shortcuts to the ResearchDrive
-RUN=run34-20190218-miseq
-OUTDIR=RHESUS20190626
+RUN=run40-20201116-miseq
+OUTDIR=reseda20201116
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
 POOLNAME=d8c24f78f9772cbdff54cf62
---
+++ README-analysis.md
@@ -0,0 +1,7 @@
+RUN=run40-20201116-miseq
+OUTDIR=reseda20201116
+
+Data analyzed with git branch run40-20201116-miseq-reseda20201116
+
+Data location: https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/run40-20201116-miseq
+
