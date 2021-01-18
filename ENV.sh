159002fb6b7f0f3d6499a2a4902e24913919c68c
git@van-schaik.org <git@van-schaik.org>
2021-01-18 18:41:16 +0100

    Script to initialize dataset specific git branch, create _GIT.md _CONTENT.md _README-analysis.md files, and upload it to the ResearchDrive

--- ENV.sh
+++ ENV.sh
@@ -1,4 +1,4 @@
-# Source this file to have shortcuts to the ResearchDrive
-RUN=runNN-2020MMDD-miseq
-OUTDIR=2020MMDD-reseda
+# Source this file to have shortcuts to the webdav server (ResearchDrive)
+RUN=runNN-2021MMDD-miseq
+OUTDIR=processing/2021MMDD-reseda
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
