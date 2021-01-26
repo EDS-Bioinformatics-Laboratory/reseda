36bf3128d9d5c5078905ce7f4af16c87b2fdda2c
Barbera van Schaik <git@van-schaik.org>
2021-01-26 13:05:40 +0100

    MergeHeavyLight.ipynb: added mutation info

--- ENV.sh
+++ ENV.sh
@@ -1,4 +1,4 @@
 # Source this file to have shortcuts to the ResearchDrive
 RUN=runXXX-20210107-SC-Tom
-OUTDIR=20210125-reseda
+OUTDIR=processing/20210125-reseda
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
