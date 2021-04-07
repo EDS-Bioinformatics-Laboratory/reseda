187aa8088e36213baad7f2f42903db52a3b86670
Barbera van Schaik <git@van-schaik.org>
2021-04-07 13:36:50 +0200

    environment variables for this analysis: ENV.sh

--- ENV.sh
+++ ENV.sh
@@ -1,4 +1,4 @@
 # Source this file to have shortcuts to the webdav server (ResearchDrive)
 RUN=run40-20201116-miseq
-OUTDIR=20201123-reseda-race
+OUTDIR=20210407-reseda-race-lightchains
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
