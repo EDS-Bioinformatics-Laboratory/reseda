4d5f583f9f2fe15a01453957d20034a061718fba
git@van-schaik.org <git@van-schaik.org>
2021-06-10 16:07:13 +0200

    Made a start searching for N-glycosylation sites in specific regions of the receptor (FWRs and CDRs)

--- ENV.sh
+++ ENV.sh
@@ -1,4 +1,4 @@
 # Source this file to have shortcuts to the ResearchDrive
 RUN=runXXX-20201021-basta
-OUTDIR=autoreactive20201021
+OUTDIR=processing/20210610-basta-n-glycosylation
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
