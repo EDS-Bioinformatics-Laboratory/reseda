2fbb96581daa7be0dd0a69dea8dfac3a0a7c1d1e
git@van-schaik.org <git@van-schaik.org>
2022-01-17 13:01:49 +0100

    ENV.sh and MakeDatasheetFromPT.ipynb for project Dornatien ADAlimumab

--- ENV.sh
+++ ENV.sh
@@ -1,4 +1,4 @@
 # Source this file to have shortcuts to the webdav server (ResearchDrive)
-RUN=runNN-2021MMDD-miseq
-OUTDIR=processing/2021MMDD-reseda
+RUN=project-20210117-ADAlimumab
+OUTDIR=processing/20220117-reseda
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
