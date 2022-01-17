facb690d4a8f3e6ff0d6ca89216b5b482ee6652d
git@van-schaik.org <git@van-schaik.org>
2022-01-17 13:05:41 +0100

    It is 2022, not 2021. I will keep the git branch as it is

--- ENV.sh
+++ ENV.sh
@@ -1,4 +1,4 @@
 # Source this file to have shortcuts to the webdav server (ResearchDrive)
-RUN=project-20210117-ADAlimumab
+RUN=project-20220117-ADAlimumab
 OUTDIR=processing/20220117-reseda
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
