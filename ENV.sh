e1d9ab30f4b5f6b97273064d6676f262d90e1ff0
git@van-schaik.org <git@van-schaik.org>
2022-01-17 14:45:51 +0100

    environment for re analysis ADA samples (to fetch the nucleotide cdr3 sequences)

--- ENV.sh
+++ ENV.sh
@@ -1,4 +1,4 @@
 # Source this file to have shortcuts to the webdav server (ResearchDrive)
-RUN=runNN-2021MMDD-miseq
-OUTDIR=processing/2021MMDD-reseda
+RUN=project-20220118-ADA-nucleotides
+OUTDIR=processing/20220118-reseda
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
