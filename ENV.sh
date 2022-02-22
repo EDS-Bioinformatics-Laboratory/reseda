9da0d9160306b30a4b91a1ad31e44dbfbb11455a
Barbera <b.d.vanschaik@amsterdamumc.nl>
2022-02-22 08:43:04 +0000

    ENV for run45

--- ENV.sh
+++ ENV.sh
@@ -1,4 +1,4 @@
 # Source this file to have shortcuts to the webdav server (ResearchDrive)
-RUN=runNN-2022MMDD-miseq
-OUTDIR=processing/2022MMDD-reseda
+RUN=run45-20220221-miseq
+OUTDIR=processing/20220222-reseda
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
