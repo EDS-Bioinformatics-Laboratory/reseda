4fbadc990777c4488258489f7bebadea515b2c15
git@van-schaik.org <git@van-schaik.org>
2022-05-02 11:49:59 +0200

    new processing subdir

--- ENV.sh
+++ ENV.sh
@@ -1,4 +1,4 @@
 # Source this file to have shortcuts to the webdav server (ResearchDrive)
 RUN=project-20220420-linda
-OUTDIR=processing/20220420-reseda
+OUTDIR=processing/20220426-reseda
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
