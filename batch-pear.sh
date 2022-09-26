3bb0542a76db3dfca13613d306e37f88b3920c9d
Barbera <b.d.vanschaik@amsterdamumc.nl>
2022-09-26 11:46:50 +0000

    adapted batch-pear.sh: slightly different input file names

--- batch-pear.sh
+++ batch-pear.sh
@@ -4,10 +4,12 @@ starttime=`date +%s`
 
 samples=$@  # get all arguments
 
+# TTTGTGTC_S1_L001_R1_001-mapped.fastq.gz
+
 for s in $samples; do
     mydir=`dirname $s`
-    prefix=`basename $s _R1_001.fastq.gz`
-    ./pear-0.9.6-bin-64 -f ${mydir}/${prefix}_R1_001.fastq.gz -r ${mydir}/${prefix}_R2_001.fastq.gz -o ${prefix} > ${prefix}-pear.log 2> ${prefix}-pear.err
+    prefix=`basename $s _L001_R1_001-mapped.fastq.gz`
+    ./pear-0.9.6-bin-64 -f ${mydir}/${prefix}_L001_R1_001-mapped.fastq.gz -r ${mydir}/${prefix}_L001_R2_001-mapped.fastq.gz -o ${prefix} > ${prefix}-pear.log 2> ${prefix}-pear.err
     gzip -f ${prefix}.assembled.fastq
 done
 
