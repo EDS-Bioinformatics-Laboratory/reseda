b3f475b0083593f21788d7c4f46ea4009ec611ce
Barbera <b.d.vanschaik@amsterdamumc.nl>
2022-10-11 08:59:27 +0000

    variable for the output directory

--- batch-align-vasaseq.sh
+++ batch-align-vasaseq.sh
@@ -9,7 +9,7 @@ for myfile in $myfiles; do
   echo "bwa mem ${ref} $myfile > ${outdir}/${prefix}.sam"
   bwa mem ${ref} $myfile > ${outdir}/${prefix}.sam
   wait
-  samtools view -F 0x04 ${outdir}/${prefix}.sam > align/${prefix}-mapped.sam
+  samtools view -F 0x04 ${outdir}/${prefix}.sam > ${outdir}/${prefix}-mapped.sam
   wait
   rm -f ${outdir}/${prefix}.sam
 done
