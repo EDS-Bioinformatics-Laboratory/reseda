da2a79c2b81a8b4174bf251bfcb9c06fb1e71c00
Barbera <b.d.vanschaik@amsterdamumc.nl>
2023-02-08 10:53:38 +0000

    FastqGetUfi.py added

--- FastqGetUfi.py
+++ FastqGetUfi.py
@@ -6,8 +6,9 @@ from Bio import SeqIO
 myfiles = sys.argv[1:]
 
 for fastqFile in myfiles:
-  fhOut = fastqFile.replace(".fastq.gz", "-ufis.csv")
+  fhOut = open(fastqFile.replace(".fastq.gz", "-ufis.csv"), "w")
   for record in SeqIO.parse(gzip.open(fastqFile, "rt"), "fastq"):
+#  for record in SeqIO.parse(open(fastqFile), "fastq"):
     sequence = str(record.seq).upper()
     ufi = sequence[:6]
     print(record.id, ufi, sep="\t", file=fhOut)
