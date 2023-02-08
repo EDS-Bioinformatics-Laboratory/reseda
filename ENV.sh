f7fb8b7d5d874253cb76ac5c5a749514a4e75349
Barbera <b.d.vanschaik@amsterdamumc.nl>
2023-02-08 10:23:52 +0000

    updated ENV.sh

--- ENV.sh
+++ ENV.sh
@@ -1,4 +1,4 @@
 # Source this file to have shortcuts to the webdav server (ResearchDrive)
 RUN=runXX-20220412-mathieu-vasaseq
-OUTDIR=processing/20220829-align-V-gene
+OUTDIR=processing/20230206-align-R2-VJC
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
