66e156304c0d9839bd7cd8503e89602f3a706499
Barbera <b.d.vanschaik@amsterdamumc.nl>
2023-06-01 11:40:46 +0000

    added argument for barcodes to process: sample-sequences.sh

--- sample-sequences.sh
+++ sample-sequences.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+barcodes=$@
 ssize="500 1000 5000 10000 50000 100000 500000 1000000"
 
 # -s seed
-for s in $ssize; do
-  echo "Sample ${s}"
+for b in $barcodes; do
+  for s in $ssize; do
+    echo "Barcode ${b} Sample ${s}"
 
-  seqtk sample -s${s} split-R2/TACTGGTA_S1_L001_R2_001.fastq.gz ${s} > TACTGGTA-${s}_S1_L001_R2_001.fastq
-  wait
+    seqtk sample -s${s} split-R2/${b}_S1_L001_R2_001.fastq.gz ${s} > ${b}-${s}_S1_L001_R2_001.fastq
+    wait
 
-  gzip TACTGGTA-${s}_S1_L001_R2_001.fastq
-  wait
+    gzip ${b}-${s}_S1_L001_R2_001.fastq
+    wait
+  done
 done
 
 echo "FINISHED"
