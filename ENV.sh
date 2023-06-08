bf5225ea120d04c717d3a539eb05e6bd0df613e4
git@van-schaik.org <git@van-schaik.org>
2023-06-08 13:05:24 +0200

    initializing project

--- ENV.sh
+++ ENV.sh
@@ -1,4 +1,4 @@
 # Source this file to have shortcuts to the webdav server (ResearchDrive)
-RUN=runNN-2022MMDD-miseq
-OUTDIR=processing/2022MMDD-reseda
+RUN=project-20230608-nanopore-hla
+OUTDIR=processing/20230608-hla
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
