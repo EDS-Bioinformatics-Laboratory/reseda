5f3935a2f06121c18f3ab96e8ff25e07ad8895f1
git@van-schaik.org <git@van-schaik.org>
2023-06-15 11:35:37 +0200

    template for ENV.sh

--- ENV.sh
+++ ENV.sh
@@ -1,6 +1,5 @@
 # Source this file to have shortcuts to the webdav server (ResearchDrive)
-#RUN=runNN-2022MMDD-miseq
-RUN=fss-test-20230613-1
-OUTDIR=20230613-reseda
+RUN=runNN-2023MMDD-miseq
+OUTDIR=2023MMDD-reseda
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
 
