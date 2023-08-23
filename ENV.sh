af2ab13191dc1e7563dbf99e279e5e1b736ac344
git@van-schaik.org <git@van-schaik.org>
2023-08-23 13:08:33 +0200

    ENV for run52

--- ENV.sh
+++ ENV.sh
@@ -1,5 +1,5 @@
 # Source this file to have shortcuts to the webdav server (ResearchDrive)
-RUN=runNN-2023MMDD-miseq
-OUTDIR=2023MMDD-reseda
+RUN=run52-20230823-miseq
+OUTDIR=20230823-reseda
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
 
