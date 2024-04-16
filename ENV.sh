5d3637a870b5e66cd9ed769893dcd6fa06834d44
Barbera <b.d.vanschaik@amsterdamumc.nl>
2024-04-16 12:06:41 +0200

    new output directory for run54

--- ENV.sh
+++ ENV.sh
@@ -1,5 +1,5 @@
 # Source this file to have shortcuts to the webdav server (ResearchDrive)
 RUN=run54-20240227-miseq
-OUTDIR=20240227-reseda
+OUTDIR=20240416-reseda
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
 
