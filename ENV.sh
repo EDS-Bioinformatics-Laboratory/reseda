b175a6bce847006c088fa1c033965799034ceda7
git@van-schaik.org <git@van-schaik.org>
2024-05-14 11:21:40 +0200

    files for run55

--- ENV.sh
+++ ENV.sh
@@ -1,5 +1,5 @@
 # Source this file to have shortcuts to the webdav server (ResearchDrive)
-RUN=runNN-2024MMDD-miseq
-OUTDIR=2024MMDD-reseda
+RUN=run55-20240514-miseq
+OUTDIR=20240514-reseda
 WEBDAV=https://researchdrive.surfsara.nl/remote.php/webdav/amc-immunogenomics/RUNS/${RUN}
 
